04e902064bf20befbb325037af837190d671a10b
stevencarlislewalker <stevencarlislewalker@gmail.com>
2025-10-16 22:50:03 -0400

    reorder figures again

--- Fig1.R
+++ Fig1.R
@@ -1,188 +1,25 @@
 source("conflicts-policy.R")
-library(sf)
-library(ggplot2)
-library(dplyr)
-library(stringr)
-library(readr)
-library(rnaturalearth)
-library(rnaturalearthdata)
-library(patchwork)
 library(magick)
 
-filename = "Fig1.tif"  ## file produced by this script
-fig_width = 5.2  ## recommended by PLoS
-fig_height = fig_width/2.6  ## trial and error
+filename = "Fig1.tif"
 
-# Metadata on the Provinces
-#
-# Columns:
-#   * postal : Provincial abbreviation
-#   * lx : Longitude in degrees
-#   * ly : Latitude in degrees
-#   * use_leader : Should the province be pointed at from outside?
-#   * region : https://www150.statcan.gc.ca/n1/en/pub/12-571-x/12-571-x2021001-eng.pdf?st=ihui0H05
-prov_locations <- read_csv("prov-locations.csv")
+scan = ("cdi_ca_1965-01-09-scan.png"
+  |> magick::image_read(density = 600)
+  |> image_scale("1898x1169")
+)
+excel = magick::image_read("cdi_ca_1965-01-09-digitization.png", density = 600)
+fig = magick::image_append(c(scan, excel), stack = TRUE)
 
-region_palette = region_palette_wrap = setNames(
-  c(
-        "#8da0cb" ## Atlantic
-      , "#66c2a5" ## Quebec
-      , "#fc8d62" ## Ontario
-      , "#ffd92f" ## Prairies
-      , "#a6d854" ## British Columbia
-      , "#e78ac3" ## Territories
-  )
-  , unique(prov_locations$region)
-)
-names(region_palette_wrap) = str_wrap(names(region_palette), 1)
+if (interactive()) {
+  magick::image_info(excel)
+  magick::image_info(scan)
+  magick::image_info(fig)
+}
 
-# Use Lambert projection as recommended by
-# https://www12.statcan.gc.ca/census-recensement/2021/ref/dict/az/Definition-eng.cfm?ID=geo031
-label_coords <- (prov_locations
-  |> st_as_sf(coords = c("lx", "ly"), crs = 4326) # map projection: https://epsg.io/?q=4326
-  |> st_transform(crs = 3347) # map projection: https://epsg.io/?q=3347
-  |> st_coordinates()
-)
-label_data = (prov_locations
-  |> mutate(
-      lx = label_coords[,"X"]
-    , ly = label_coords[,"Y"]
-  )
-)
-
-# Point both to the island of Newfoundland
-# and to Labrador.
-nl_coords <- (tibble(
-      lx = rep(label_data$lx[label_data$postal == "NL"], 2)
-    , ly = rep(label_data$ly[label_data$postal == "NL"], 2)
-    , cx = c(-55.8, -62.5)
-    , cy = c(48.5, 53.5)
-  )
-  |> st_as_sf(coords = c("cx", "cy"), crs = 4326) # map projection: https://epsg.io/?q=4326
-  |> st_transform(crs = 3347) # map projection: https://epsg.io/?q=3347
-)
-nl_coords$cx = st_coordinates(nl_coords)[,1]
-nl_coords$cy = st_coordinates(nl_coords)[,2]
-nl_targets = mutate(nl_coords
-  , lx = rep(label_data$lx[label_data$postal == "NL"], 2)
-  , ly = rep(label_data$ly[label_data$postal == "NL"], 2)
-)
-
-## apply the Lambert projection and join the locations of
-## the province labels on the map
-map_data <- (
-     ne_states(country = "canada", returnclass = "sf")
-  |> st_transform(crs = 3347) # map projection: https://epsg.io/?q=3347
-  |> mutate(centroid = st_centroid(geometry))
-  |> mutate(
-      cx = st_coordinates(centroid)[, 1]
-    , cy = st_coordinates(centroid)[, 2]
-  )
-  |> select(-region)
-  |> left_join(label_data, by = "postal")
-)
-
-## plot object contain only the map (without the legend)
-map_plot <- (map_data
-  |> mutate(region = str_wrap(region, width = 1))
-  |> mutate(region = factor(region, levels = names(region_palette_wrap)))
-  |> ggplot()
-  + geom_sf(aes(fill = region), color = "black", linewidth = 0.1)
-  + geom_segment(
-      data = filter(map_data, use_leader & postal != "NL")
-    , aes(x = lx, y = ly, xend = cx, yend = cy) # point from label to centroid
-    , color = "gray30", linewidth = 0.2
-  )
-  + geom_segment(
-      data = nl_targets
-    , aes(x = lx, y = ly, xend = cx, yend = cy)
-    , color = "gray30", linewidth = 0.2
-  )
-  + geom_label(
-      aes(x = lx, y = ly, label = postal)
-    , size = 2.5, label.size = 0.25, label.padding = unit(0.2, "lines")
-    , fontface = "bold"
-  )
-  + scale_fill_manual(values = region_palette_wrap)
-  + theme_minimal()
-  + theme(
-      axis.title = element_blank()
-    , axis.text = element_blank()
-    , axis.ticks = element_blank()
-    , panel.grid = element_blank()
-    , plot.margin = margin(0, 0, 0, 0)
-  )
-  + guides(fill = guide_legend(title = "Region"))
-)
-
-## object for translating iso-3166-2 codes into full
-## province and territory names
-translate = (map_data
-  |> data.frame()
-  |> select(postal, name)
-  |> with(setNames(name, postal))
-)
-
-## plot object containing only the legend
-text_plot = (prov_locations
-  |> mutate(label = sprintf("%s: %s", postal, translate[postal]))
-  |> mutate(x = 0, y = rev(seq_along(prov_locations$postal)))
-  |> ggplot()
-  + geom_text(
-      aes(x + 0.1, y, label = label)
-    , hjust = 0
-    , size = 2.8
-    , family = "Helvetica"
-  )
-  + geom_tile(
-      aes(
-          x = x + 0.05
-        , y = y
-        , height = 1
-        , width = 0.05
-        , fill = region
-      )
-    , linewidth = 0.1
-    , colour = "black"
-  )
-  + scale_colour_identity()
-  + scale_fill_manual(values = region_palette)
-  + coord_cartesian(xlim = c(0, 1), expand = FALSE, clip = "off")
-  + guides(fill = "none")
-  + theme_void()
-  + theme(
-      plot.margin = margin(0, 0, 0, 0)
-    , axis.title = element_blank()
-    , axis.title.x = element_blank()
-    , axis.title.y = element_blank()
-    , axis.text = element_blank()
-    , axis.ticks = element_blank()
-    , panel.grid = element_blank()
-  )
-)
-
-
-full_plot = (
-    map_plot
-  + text_plot
-  + plot_layout(
-      widths = c(2, 1.8)
-    , heights = 1
-  )
-)
-
-ggsave(filename
-  , full_plot
-  , device = "tif"
-  , height = fig_height
-  , width = fig_width
-  , units = "in"
-  , dpi = 600
-)
-(filename
-  |> image_read()
-  |> image_write(filename
-    , compression = "LZW"
-    , flatten = TRUE
-  )
+magick::image_write(fig
+  , filename
+  , flatten = TRUE
+  , compression = "LZW"
+  , format = "tiff"
+  , density = 600
 )
--- Fig2.R
+++ Fig2.R
@@ -1,123 +1,50 @@
 source("conflicts-policy.R")
-library(dplyr)
-library(iidda.analysis)
-library(iidda)
-library(lubridate)
-library(ggplot2)
-library(patchwork)
-library(cowplot)
-library(Cairo)
 library(magick)
+library(ggplot2)
+library(grid)
+library(patchwork)
 
-n_pages = 3    ## number of figures in this series
-first_page = 2 ## number labelling the first figure in this series
+filename = "Fig2.tif"  ## file produced by this script
 
-canmod_cdi_normalized = readRDS("canmod-cdi-normalized.rdata")
-
-
-normalized_for_extent_plot = (canmod_cdi_normalized
-  |> mutate(time_scale = ifelse(time_scale == "2wk", "wk", time_scale))
-  |> mutate(time_scale = ifelse(time_scale == "3qr", "qr", time_scale))
-  |> mutate(time_scale = case_when(
-        time_scale == "wk" ~ "weekly"
-      , time_scale == "2wk" ~ "2-weekly"
-      , time_scale == "mo" ~ "monthly"
-      , time_scale == "qr" ~ "quarterly"
-      , time_scale == "3qr" ~ "3-quarterly"
-  ))
-
-  |> iidda_defaults(
-        count_variable = "cases_this_period"
-      , norm_variable = "population"
-      , period_start_variable = "period_start_date"
-      , period_end_variable = "period_end_date"
-      , period_mid_time_variable = "period_mid_time"
-      , period_mid_date_variable = "period_mid_date"
-      , period_days_variable = "num_days"
-      , among_panel_variable = "basal_disease"
-      , within_panel_variable = "iso_3166_2"
-      , colour_variable = "time_scale"
-  )
+image_good = magick::image_read(
+    "cdi_ery_gon_on_1939_easy_to_read.png"
+  , density = 600
+)
+image_bad = magick::image_read(
+    "cdi_poliounspec_ca_1955_difficult_to_read.png"
+  , density = 600
 )
 
-n_all_diseases = length(unique(normalized_for_extent_plot$basal_disease))
-diseases_per_page = ceiling(n_all_diseases / n_pages)
-remainder = (diseases_per_page * n_pages) - n_all_diseases
+grob_good = grid::rasterGrob(image_good, hjust = 0.6, vjust = 0.5)
+grob_bad = grid::rasterGrob(image_bad, hjust = 0.6, vjust = 0.5)
 
+p = ggplot()
+p_good = p + annotation_custom(grob_good)
+p_bad = p + annotation_custom(grob_bad)
 
-plots = iidda_availability(normalized_for_extent_plot
-  , pages = seq_len(n_pages)
-  , page_size = diseases_per_page
-  , scale_colour = c(
-        weekly    = "#007FFF"
-      , `2-weekly` = "limegreen"
-      , monthly   = "#FF7F00"
-      , quarterly = "#DC143C"
-      , `3-quarterly` = "purple"
-    )
-  , title_colour = "Time Scale:"
-  , title_totals = "Total\nCases"
-  , within_panel_order = c(
-        ## east
-          "CA-NL"
-        , "CA-PE"
-        , "CA-NS"
-        , "CA-NB"
-        ## central
-        , "CA-QC"
-        , "CA-ON"
-        ## west
-        , "CA-MB"
-        , "CA-SK"
-        , "CA-AB"
-        , "CA-BC"
-        ## north
-        , "CA-YT"
-        , "CA-NT"
-        , "CA-NU"
-      ) |> rev()
-  , colour_order = c("weekly", "2-weekly", "monthly", "quarterly", "3-quarterly")
-  , x_title = "Year"
-  , x_breaks = seq(
-        as.POSIXct("1910-01-01")
-      , as.POSIXct("2010-01-01")
-      , by = "20 years"
-    )
-  , x_minor_breaks = seq(
-        as.POSIXct("1910-01-01")
-      , as.POSIXct("2010-01-01")
-      , by = "5 years"
-    )
-  , x_date_labels = "%Y"
-  , text_size = 9
-  , left_margin = 100
-  , legend_margin = 15
-  , subplot_widths = c(5, 1)
+plot = (
+    plot_spacer()
+  + p_good
+  + labs(caption = "(A) Easy to read")
+  + theme_void()
+  + theme(plot.caption = element_text(hjust = 0, size = 12))
+  + patchwork::plot_spacer()
+  + p_bad
+  + labs(caption = "(B) Difficult to read")
+  + theme_void()
+  + theme(plot.caption = element_text(hjust = 0, size = 12))
+  + patchwork::plot_spacer()
+  + patchwork::plot_layout(ncol = 5, widths = c(-0.04, 1, -0.1, 1.25, -0.2))
 )
-
-save_plots = function(plots, first_page) {
-  for (page in seq_len(n_pages)) {
-    filename = sprintf("Fig%s.tif", page + first_page - 1L)
-    tiff(filename
-      , width = 7.5
-      , height = 8.75
-      , units = "in"
-      , res = 600 ## plos requires 300-600
-    )
-    plot(plots[[page]])
-    dev.off()
-    (filename
-      |> image_read()
-      |> image_write(filename, compression = "LZW", flatten = TRUE)
-    )
-  }
-}
-
-harmonized_plots = harmonize_plots(plots)
-save_plots(harmonized_plots, first_page = first_page)
-
-for (i in seq_len(n_pages)) {
-  nn = diseases_per_page
-  if (i == n_pages) nn = nn - remainder
-  writeLines(as.character(nn), sprintf("n-diseases-page-%s", i))
-}
+ggsave(filename
+  , plot
+  , device = "tiff"
+  , dpi = 600
+  , width = 6
+  , height = 6
+  , units = "in"
+)
+(filename
+  |> magick::image_read()
+  |> magick::image_write(filename, compression = "LZW", flatten = TRUE)
+)
